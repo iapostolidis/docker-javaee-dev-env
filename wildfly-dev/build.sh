ef8da17040530981e29c7e39c0ae800129b63353
Ioannis Apostolidis <pantsos@gmail.com>
2019-05-11 10:45:01 +0200

    minor wildfly changes

--- wildfly-dev/build.sh
+++ wildfly-dev/build.sh
@@ -1,4 +1,6 @@
 #/bin/bash
 
-docker build --pull --no-cache -t pantsoft.de:5000/wildfly-dev .
+# docker build --pull --no-cache -t pantsoft.de:5000/wildfly-dev .
+
+docker build -t pantsoft.de:5000/wildfly-dev .
 exit 0
